bf30459799bd8ba146a89bd6a93e4ec6756a8b3f
Madeline Grenier <madeline.grenier@mymail.champlain.edu>
2024-05-17 15:02:02 -0400

    bubbleAnims working accordignly now

--- Aquarius Pixel Jam/Assets/Scripts/Customers.cs
+++ Aquarius Pixel Jam/Assets/Scripts/Customers.cs
@@ -6,23 +6,64 @@ public class Customers : MonoBehaviour
 {
     public string Name;
     public bool isBig;
-    public Animation walkAnim;
+    public Animator walkAnim;
+    private Animator bubbleAnim;
+    int seatChosen = 1;
+    private SpriteRenderer spriteRenderer;
     public Sprite sitting;
     public GameObject cup;
     public int vol;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        walkAnim = GetComponent<Animator>();
+        GameObject bubbleObject = GameObject.Find("seat" + seatChosen + "Bubble");
+        bubbleAnim = bubbleObject.GetComponent<Animator>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            if (bubbleAnim.GetBool("IsEnter"))
+            {
+                StartMiniGame();
+            }
+        }
     }
+
     public void StartOrder()
     {
-        Debug.Log("Order started");
+        spriteRenderer.sprite = sitting;
+        Debug.Log(Name + " is ready to order!");
+        walkAnim.enabled = false;
+        bubbleAnim.SetTrigger("bubbleAppear");
+    }
+
+    // OnTriggerEnter is called when the Collider other enters the trigger
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("entered circle");
+        if (other.gameObject.tag == "Player")
+        {
+            bubbleAnim.SetBool("IsEnter", true);
+        }
+    }
+
+    // OnTriggerExit is called when the Collider other exits the trigger
+    void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.gameObject.tag == "Player")
+        {
+            bubbleAnim.SetBool("IsEnter", false);
+        }
+    }
+
+    void StartMiniGame()
+    {
+        Debug.Log(Name + " is thirsty!");
     }
 }
